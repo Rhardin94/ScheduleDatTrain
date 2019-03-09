b3324fa1dbe3d15a01da5f059cfc8b730a3a3088
Robert H <robertjameshardin@aol.com>
2019-03-09 01:05:47 -0600

    modified initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCuh8LmWDUI6tyuyU4qx2RXnTgRqwH-ll8",
+    authDomain: "train-schedules-e7aef.firebaseapp.com",
+    databaseURL: "https://train-schedules-e7aef.firebaseio.com",
+    projectId: "train-schedules-e7aef",
+    storageBucket: "",
+    messagingSenderId: "762110917052"
+};
+firebase.initializeApp(config);
